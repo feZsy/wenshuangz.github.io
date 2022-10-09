110717213036c5154869dec03158793e398e1706
fezws <wenshuangz@gmail.com>
2022-10-09 14:10:13 +0800

    tset

---
+++ docs/hel/@frontend/hel-sy-test-lib-private-homepage/v1/precache-manifest.9d38094c4277cc4db6be38dd5c9f1268.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "272df3cf21835307fd0d3d1ecff4a1e6",
+    "url": "https://wenshuangz.github.io/docs/hel/@frontend/hel-sy-test-lib-private-homepage/v1/index.html"
+  },
+  {
+    "revision": "fde21a5253bc98b01a47",
+    "url": "https://wenshuangz.github.io/docs/hel/@frontend/hel-sy-test-lib-private-homepage/v1/static/js/0.ca2caa71.chunk.js"
+  },
+  {
+    "revision": "114fa89da3636722bc04f5ae14df1de3",
+    "url": "https://wenshuangz.github.io/docs/hel/@frontend/hel-sy-test-lib-private-homepage/v1/static/js/0.ca2caa71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6349d71b54643db0ca38",
+    "url": "https://wenshuangz.github.io/docs/hel/@frontend/hel-sy-test-lib-private-homepage/v1/static/js/main.48d85af4.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "https://wenshuangz.github.io/docs/hel/@frontend/hel-sy-test-lib-private-homepage/v1/static/js/main.48d85af4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006c997e448b3a58d46e",
+    "url": "https://wenshuangz.github.io/docs/hel/@frontend/hel-sy-test-lib-private-homepage/v1/static/js/runtime-main.44e0c669.js"
+  }
+]);
